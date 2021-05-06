e16d813adf4dbe372d661625792ff50e5d2564b2
Kevin Andres <kevin.andres@motorolasolutions.com>
2021-05-05 21:02:43 -0600

    initial commit

---
+++ TestWebApp/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TestWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get() => new[] { "A", "B", "C" };
+    }
+}
